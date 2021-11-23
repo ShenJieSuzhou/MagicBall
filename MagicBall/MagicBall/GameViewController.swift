61807da4ad293edef88f737dab09153bbcc79e34
ShenJieSuzhou <745689122@qq.com>
2021-11-23 15:33:24 +0800

    Finish a ball moving demo.

--- MagicBall/MagicBall/GameViewController.swift
+++ MagicBall/MagicBall/GameViewController.swift
@@ -6,40 +6,162 @@
 //
 
 import UIKit
-import MetalKit
+import SceneKit
 
 // Our iOS specific view controller
 class GameViewController: UIViewController {
 
-    var renderer: Renderer!
-    var mtkView: MTKView!
+    let CategoryTree = 2
+    
+    var sceneView:SCNView!
+    var scene:SCNScene!
+    
+    var ballNode:SCNNode!
+    var selfieStickNode:SCNNode!
+    
+    var motion = MotionHelper()
+    var motionForce = SCNVector3(0, 0, 0)
+    
+    var sounds:[String:SCNAudioSource] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        guard let mtkView = view as? MTKView else {
-            print("View of Gameview controller is not an MTKView")
-            return
-        }
-
-        // Select the device to render with.  We choose the default device
-        guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
-            print("Metal is not supported")
-            return
-        }
+        setupScene()
+        setupNodes()
+        setupSounds()
+    }
+    
+    func setupScene(){
+        sceneView = self.view as! SCNView
+        sceneView.delegate = self
         
-        mtkView.device = defaultDevice
-        mtkView.backgroundColor = UIColor.black
-
-        guard let newRenderer = Renderer(metalKitView: mtkView) else {
-            print("Renderer cannot be initialized")
-            return
+        //sceneView.allowsCameraControl = true
+        scene = SCNScene(named: "art.scnassets/MainScene.scn")
+        sceneView.scene = scene
+        
+        scene.physicsWorld.contactDelegate = self
+        
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.numberOfTapsRequired = 1
+        tapRecognizer.numberOfTouchesRequired = 1
+        
+        tapRecognizer.addTarget(self, action: #selector(GameViewController.sceneViewTapped(recognizer:)))
+        sceneView.addGestureRecognizer(tapRecognizer)
+    }
+    
+    func setupNodes() {
+        ballNode = scene.rootNode.childNode(withName: "ball", recursively: true)!
+        ballNode.physicsBody?.contactTestBitMask = CategoryTree
+        selfieStickNode = scene.rootNode.childNode(withName: "selfieStick", recursively: true)!
+    }
+    
+    func setupSounds() {
+        let sawSound = SCNAudioSource(fileNamed: "chainsaw.wav")!
+        let jumpSound = SCNAudioSource(fileNamed: "jump.wav")!
+        sawSound.load()
+        jumpSound.load()
+        sawSound.volume = 0.3
+        jumpSound.volume = 0.4
+        
+        sounds["saw"] = sawSound
+        sounds["jump"] = jumpSound
+        
+        let backgroundMusic = SCNAudioSource(fileNamed: "background.mp3")!
+        backgroundMusic.volume = 0.1
+        backgroundMusic.loops = true
+        backgroundMusic.load()
+        
+        let musicPlayer = SCNAudioPlayer(source: backgroundMusic)
+        ballNode.addAudioPlayer(musicPlayer)
+        
+    }
+    
+    @objc func sceneViewTapped (recognizer:UITapGestureRecognizer) {
+        let location = recognizer.location(in: sceneView)
+        
+        let hitResults = sceneView.hitTest(location, options: nil)
+        
+        if hitResults.count > 0 {
+            let result = hitResults.first
+            if let node = result?.node {
+                if node.name == "ball" {
+                    let jumpSound = sounds["jump"]!
+                    ballNode.runAction(SCNAction.playAudio(jumpSound, waitForCompletion: false))
+                    ballNode.physicsBody?.applyForce(SCNVector3(x: 0, y:4, z: -2), asImpulse: true)
+                }
+            }
         }
-
-        renderer = newRenderer
-
-        renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
-
-        mtkView.delegate = renderer
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
     }
 }
+
+extension GameViewController : SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        let ball = ballNode.presentation
+        let ballPosition = ball.position
+        
+        let targetPosition = SCNVector3(x: ballPosition.x, y: ballPosition.y + 5, z:ballPosition.z + 5)
+        var cameraPosition = selfieStickNode.position
+        
+        let camDamping:Float = 0.3
+        
+        let xComponent = cameraPosition.x * (1 - camDamping) + targetPosition.x * camDamping
+        let yComponent = cameraPosition.y * (1 - camDamping) + targetPosition.y * camDamping
+        let zComponent = cameraPosition.z * (1 - camDamping) + targetPosition.z * camDamping
+        
+        cameraPosition = SCNVector3(x: xComponent, y: yComponent, z: zComponent)
+        selfieStickNode.position = cameraPosition
+        
+        
+        motion.getAccelerometerData { (x, y, z) in
+            self.motionForce = SCNVector3(x: x * 0.05, y:0, z: (y + 0.8) * -0.05)
+        }
+        
+        ballNode.physicsBody?.velocity += motionForce
+        
+    }
+}
+
+extension GameViewController : SCNPhysicsContactDelegate {
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        var contactNode:SCNNode!
+        
+        if contact.nodeA.name == "ball" {
+            contactNode = contact.nodeB
+        }else{
+            contactNode = contact.nodeA
+        }
+        
+        if contactNode.physicsBody?.categoryBitMask == CategoryTree {
+            contactNode.isHidden = true
+            
+            let sawSound = sounds["saw"]!
+            ballNode.runAction(SCNAction.playAudio(sawSound, waitForCompletion: false))
+            
+            let waitAction = SCNAction.wait(duration: 15)
+            let unhideAction = SCNAction.run { (node) in
+                node.isHidden = false
+            }
+            
+            let actionSequence = SCNAction.sequence([waitAction, unhideAction])
+            
+            contactNode.runAction(actionSequence)
+        }
+        
+    }
+}
+
+
