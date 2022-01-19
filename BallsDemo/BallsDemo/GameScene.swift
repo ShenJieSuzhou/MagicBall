c56c87f1e4c81b8166415027e179b80bb172af5c
ShenJieSuzhou <745689122@qq.com>
2022-01-19 17:51:09 +0800

    Add particle effect。

--- BallsDemo/BallsDemo/GameScene.swift
+++ BallsDemo/BallsDemo/GameScene.swift
@@ -9,68 +9,40 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private let nodeName = "hello"
-    private var spinnyNode : SKShapeNode!
     private var lastTouch: CGPoint? = nil
     private var selected: Bool = false
+    private var joinButton: SKSpriteNode!
+    private var nodeArray: [SKSpriteNode] = []
     
     let BallCategory   : UInt32 = 0x1 << 0
     let BorderCategory : UInt32 = 0x1 << 4
     
-    var ball = SKSpriteNode()
+    var fire = SKEmitterNode(fileNamed: "Fire")
     
-    override func sceneDidLoad() {
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        self.spinnyNode.name = nodeName
-        self.spinnyNode.lineWidth = 2.5
-        self.spinnyNode?.position = CGPoint(x: 100.0, y: 100.0)
-    }
+    let colors = [UIColor.red, UIColor.green, UIColor.blue, UIColor.brown, UIColor.cyan, UIColor.orange]
+    
+    let nodeNames = ["qq", "ww", "ss", "rr", "tt", "yy", "uu"]
+    var count = 0
     
     override func didMove(to view: SKView) {
+        // 添加按钮
+        joinButton = self.childNode(withName: "JoinButton") as? SKSpriteNode
+        joinButton?.isUserInteractionEnabled = true
         
+        // 边界碰撞
         let screenBorder = SKPhysicsBody(edgeLoopFrom: self.frame)
         screenBorder.friction = 0 /// So doesn't slow down the objects that collide
         screenBorder.restitution = 1 /// So the ball bounces when hitting the screen borders
         self.physicsBody = screenBorder
-        
-        ball = self.childNode(withName: "ball") as! SKSpriteNode
-        ball.physicsBody?.applyImpulse(CGVector(dx: 100, dy: 100))
-        
-//        if let spinnyNode = self.spinnyNode {
-//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-//        }
-        
-        // 边界碰撞
-//        let screenBorder = SKPhysicsBody(edgeLoopFrom: self.frame)
-////        screenBorder.friction = 0
-////        screenBorder.restitution = 1
-//        self.physicsBody = screenBorder
-//        self.physicsBody?.categoryBitMask = BorderCategory
-//
-//        // 给球添加推力
-//        let ball = self.childNode(withName: "ball") as! SKSpriteNode
-//        ball.physicsBody?.affectedByGravity = false
-//        ball.physicsBody?.categoryBitMask = BallCategory
-//        ball.physicsBody?.contactTestBitMask = BorderCategory
-//        ball.physicsBody?.collisionBitMask = BorderCategory
-//        ball.physicsBody?.restitution = 1.0
-//        ball.physicsBody?.friction = 0
-//        ball.physicsBody?.linearDamping = 0
-//        ball.physicsBody?.angularDamping = 0
-//
-//        ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches {
             let position = t.location(in: self)
             lastTouch = position
-            if selectedNodeForTouch(touchLocation: position) {
-                self.selected = true
-            }
+//            if selectedNodeForTouch(touchLocation: position) {
+//                self.selected = true
+//            }
         }
     }
     
@@ -84,6 +56,21 @@ class GameScene: SKScene {
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         lastTouch = nil
         self.selected = false
+        
+        for touch: AnyObject in touches {
+            let location = touch.location(in: self)
+            if self.joinButton.contains(location){
+                print("+++++++++ generate new node +++++++++")
+                let random = Int(arc4random_uniform(UInt32(self.colors.count)))
+                count = count + 1
+                if count > 10 {
+                    return
+                }
+                if generateNewSpriteNode(id: String(count), name: String(count), color: colors[random]) {
+                    
+                }
+            }
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -93,38 +80,63 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-//        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-//        if ball.position.y <= self.frame.minY{
-//            ball.physicsBody?.applyImpulse(CGVector(dx: 20 * (-1), dy: 20 * (-1)))
+        //print("position: x = \(self.ball.position.x)  y = \(self.ball.position.y)")
+        // socket 发送坐标
+    }
+    
+    
+    
+    /// 生成新的节点
+    /// - Parameters:
+    ///   - id: 标识
+    ///   - name: 名称
+    ///   - color: 颜色
+    /// - Returns: 是否成功
+    func generateNewSpriteNode(id: String, name: String, color: UIColor) -> Bool{
+        let node = SKSpriteNode(color: color, size: CGSize(width: 30, height: 30))
+        node.name = id
+        node.position = CGPoint(x: -100, y: 100)
+        node.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        node.physicsBody = SKPhysicsBody(circleOfRadius: 30)
+        node.physicsBody?.isDynamic = true
+        node.physicsBody?.restitution = 1
+        node.physicsBody?.applyImpulse(CGVector(dx: 100, dy: 20))
+        
+        fire?.targetNode = self
+        fire?.particleColorBlendFactor = 1.0
+        fire?.particleColorSequence = nil
+        fire?.particleColor = color
+        node.addChild(fire!)
+        
+        self.addChild(node)
+        nodeArray.append(node)
+        
+        return true
+    }
+    
+//    func selectedNodeForTouch(touchLocation: CGPoint) -> Bool{
+//        let touchedNode = self.atPoint(touchLocation)
+//        if touchedNode is SKShapeNode {
+//            if self.spinnyNode.isEqual(to: touchedNode) {
+//                if touchedNode.name == nodeName {
+//                    return true
+//                }
+//            }
 //        }
-//        else if ball.position.y >= self.frame.maxY {
-//            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
-//        } else if
-    }
-    
-    func selectedNodeForTouch(touchLocation: CGPoint) -> Bool{
-        let touchedNode = self.atPoint(touchLocation)
-        if touchedNode is SKShapeNode {
-            if self.spinnyNode.isEqual(to: touchedNode) {
-                if touchedNode.name == nodeName {
-                    return true
-                }
-            }
-        }
-        return false
-    }
-    
-    func randomFloat(from:CGFloat, to:CGFloat) -> CGFloat {
-      let rand:CGFloat = CGFloat(Float(arc4random()) / 0xFFFFFFFF)
-      return (rand) * (to - from) + from
-    }
-    
-    func randomDirection() -> CGFloat {
-        let speedFactor: CGFloat = 3.0
-        if self.randomFloat(from: 0.0, to: 100.0) >= 50 {
-          return -speedFactor
-        } else {
-          return speedFactor
-        }
-      }
+//        return false
+//    }
+//
+//    func randomFloat(from:CGFloat, to:CGFloat) -> CGFloat {
+//      let rand:CGFloat = CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+//      return (rand) * (to - from) + from
+//    }
+//
+//    func randomDirection() -> CGFloat {
+//        let speedFactor: CGFloat = 3.0
+//        if self.randomFloat(from: 0.0, to: 100.0) >= 50 {
+//          return -speedFactor
+//        } else {
+//          return speedFactor
+//        }
+//      }
 }
